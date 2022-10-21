c3448302c0744db1ba8a4a030dca1e414c6d7b47
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-10-21 14:10:34 +0530

    [FEAT]: added getBlogsByCategory ctrl and route

--- src/controllers/categoryCtrl.ts
+++ src/controllers/categoryCtrl.ts
@@ -55,7 +55,7 @@ const categoryCtrl = {
         {
           _id: req.params.id,
         },
-        { name: req.body.name }
+        { name: req.body.name.toLowerCase() }
       );
 
       res.json({ msg: "Update Success!" });
