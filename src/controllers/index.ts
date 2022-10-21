dc13f85b848ceb48971f5a8c78d751f2c4970e20
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-10-21 11:40:56 +0530

    [FEAT]: create blog api

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,6 @@
 import authCtrl from "./authCtrl";
 import userCtrl from "./userCtrl";
 import categoryCtrl from "./categoryCtrl";
+import blogCtrl from "./blogCtrl";
 
-export { authCtrl, userCtrl, categoryCtrl };
+export { authCtrl, userCtrl, categoryCtrl, blogCtrl };
